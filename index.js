d43220d5a30529c0e8abfe7bf8f6a81102ac7041
Sean Mishra <santakmishra@outlook.com>
2017-03-03 00:09:07 -0600

    Getting port from heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const methodFilter = require('./middleware/method-filter')
 const root = require('./routes/root')
 const slash = require('express-slash')
+const port = process.env.PORT || 5000
 
 const app = express()
 app.use(methodFilter)
@@ -11,4 +12,4 @@ app.use(express.static('./public/'))
 app.use('/', root)
 app.use(slash())
 
-app.listen(1337, () => { console.log('Listening on port 1337') })
+app.listen(port, () => { console.log('Listening on port 1337') })
