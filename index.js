034dc1577e51ff1e7600ab3a28282b73f6797a2b
Sean Mishra <santakmishra@outlook.com>
2017-03-24 10:56:53 -0500

    Updated console message on app start

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ app.use(express.static('./public/'))
 app.use('/', root)
 app.use(slash())
 
-app.listen(port, () => { console.log('Listening on port 1337') })
+app.listen(port, () => { console.log(`Listening on port ${port}`) })
